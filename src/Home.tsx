9f2524a23dd57ef4f026a67b49c762986f72edb1
user <akbarabdurahmanov864@gmail.com>
2023-07-23 16:27:56 +0600

    welcome

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,5 @@
-
 import React from "react";
 
-const Home: React.FC = () => (
-  <h1>Welcome to your PWA!</h1>
-);
+const Home: React.FC = () => <h1>welcome hackathon participants!</h1>;
 
 export default Home;
